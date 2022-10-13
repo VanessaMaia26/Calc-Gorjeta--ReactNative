818d447a4192dd4e7966e9549fce696350cc4e8c
VanessaMaia26 <vanessamaia2607@gmail.com>
2022-10-13 12:46:57 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button } from 'react-native';
+import styled from 'styled-components/native'
 
-export default function App() {
+const Page = styled.SafeAreaView`
+flex: 1;
+align-items: center;
+`;
+
+const HeaderText = styled.Text`
+font-size: 25px;
+`;
+
+const Input = styled.TextInput`
+width: 100%;
+height: 50px;
+font-size: 18px;
+background-color: #EEE
+margin-top: 20px;
+border-radius: 10px;
+padding: 10px;
+`;
+
+const CalcButton = styled.Button`
+margin-top: 10px;
+`;
+
+const ResultArea = styled.view`
+width: 100%
+margin-top: 30px;
+background-color: #EEE;
+padding: 20px;
+justify-content: center;
+align-items: center;
+`;
+
+const ResultItemTitle = styled.Text`
+font-size: 18px;
+font-weight: bold;
+`;
+
+const ResultItem = styled.Text`
+font-size: 15px;
+margin-botoom: 30px;
+`;
+
+const PctArea = styled.View`
+flex-direction: row;
+margin: 20px;
+`;
+
+const PctIten = styled.Button`
+
+`;
+
+export default () => {
+
+  const [bill, setBill] = useState('');
+  const [tip, setTip] = useState(0);
+  const [pct, setPct] = useState(10);
+
+  const calc = () => {
+    let nBill = parseFloat(bill);
+
+    if(nBill) {
+      setTip( (pct/100) * nBill );
+    } else {
+      alert("Digite o valor da conta");
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Page>
+      <HeaderText>Calculadora de Gorjeta</HeaderText>
+      <Input
+      placeholder="Quanto deu a conta?"
+      placeholderTextColor="#000"
+      keyboardType="numeric"
+      value={bill}
+      onChangeText={n => setBill(n)}
+      />
+
+      <PctArea>
+        <PctItem title="5%" onPress={() => setPct(5)} />
+        <PctItem title="10%" onPress={() => setPct(10)} />
+        <PctItem title="15%" onPress={() => setPct(15)} />
+        <PctItem title="20%" onPress={() => setPct(20)} />
+      </PctArea>
+
+      <CalcButton title={`Calcular ${pct}%`} onPress={calc} />
+      {tip > 0 &&
+       <ResultArea>
+         <ResultItemTitle>Valor da Conta</ResultItemTitle>
+         <ResultItem>R$ {parseFloat(bill).toFixed(2)}</ResultItem>
+
+         <ResultItemTitle>valor da Gorjeta</ResultItemTitle>
+         <ResultItem>R$ {tip.toFixed(2)} ({pct}%)</ResultItem>
+
+         <ResultItemTitle>Valor Total</ResultItemTitle>
+         <ResultItem>R$ {(parseFloat(bill) + tip).toFixed(2)}</ResultItem>
+       </ResultArea>
+      }
+    </Page>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
